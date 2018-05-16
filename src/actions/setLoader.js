f85ef69970e964592c4eb25a725aa037c937d47c
vanTsymbalenko <IvanZymbalenko1@gmail.com>
2018-05-16 20:57:55 +0300

    deleted for pull request

--- src/actions/setLoader.js
+++
@@ -1,8 +0,0 @@
-import { SET_LOADER } from "../constants/appConst";
-
-export const setLoader = state => {
-  return {
-    type: SET_LOADER,
-    payload: state
-  };
-};
--- src/reducers/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { authData } from "./authData";
-import { appData } from "./appData";
-
-export default combineReducers({
-  authData,
-  appData
-});
